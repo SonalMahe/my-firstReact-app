8e68d89f9e9e4c2e901a1e267e6397869b5e00b2
Sonal Maheshwari <sonal.obedient@gmail.com>
2025-11-18 17:55:46 +0100

    updated

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,11 +6,11 @@ function About() {
             <h1>About-Me page</h1>
 
             <p>I am student learning Front End development and design.I have a strong interest in front-end design, UI/UX ,creating responsive applications. I am always eager to learn something new, creative thinker and someone who enjoys turning ideas into reality code.
-                <ul>Skills section-</ul>
+                <ul><b>Skills Section-</b></ul>
                 <li>1.HTML /CSS</li>
                 <li>2.JavaScript</li>
                 <li>3.React</li>
-                <li>4.UI/UX designs</li>
+                <li>4.UI/UX Designs</li>
                 <li>5.Problem Solving</li>
                 <li>6.Quick Learner</li>
             </p>
--- src/pages/contactus.jsx
+++ src/pages/contactus.jsx
@@ -2,7 +2,6 @@ function ContactUs() {
     return (
         <>
         <h1>Contact-us page</h1>
-
         <h4>This page is in under construction. More details coming soon.</h4>
         </>
     )
