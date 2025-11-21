734c93c7aff65a218ea55f100e365fd1817dfc08
Sonal Maheshwari <sonal.obedient@gmail.com>
2025-11-22 00:05:48 +0100

    changes in CSS
    -added image n other obj

--- src/component/PopupWindow.jsx
+++ src/component/PopupWindow.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "../component/PopupWindow.css";
 
-function PopupWindow({title, info, onClick}){
-    return(
+function PopupWindow({ title, info, tech, image, onClick }) {
+    return (
         <div className="popup-window">
             <h1 className="title">{title}</h1>
             <p className="info">{info}</p>
-           {/*<img className="image" src="../src/assets/typinggame.JPG"> {image}</img>*/}
+            <h3 className="tech">Tech used:{tech}</h3>
+            <img className="image" src={image} alt="Typing Game" />
             <button className="close-button" onClick={onClick}> Close Popup</button>
-
         </div>
     );
 };
--- src/pages/PopUp.jsx
+++ src/pages/PopUp.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import PopupWindow from "../component/PopupWindow.jsx";
+import TypingImage from "../assets/typinggame.JPG"
 
 function Popup() {
     const [popupInfo, setPopupInfo] = useState({
         title: " Typing Word Game",
-        info: "This typing game is created by JavaScript using different types of DOM elements ex- getElementById, Math function ,addEventListener, Arrays and objects, SetInterval, clearInterval and Update score",
-        image: "here is image",
+        info: "This typing game is created by JavaScript and I used different types of DOM elements ex- getElementById, Math function ,addEventListener, Arrays and objects, SetInterval, clearInterval and Update score",
+        tech: "JavaScript(DOM) & CSS",
+        image: TypingImage,
     });
+    
     const [isOpen, setIsOpen] = useState(false);
     const closepopup = () => {
         setIsOpen(false)
@@ -21,6 +24,8 @@ function Popup() {
                 <PopupWindow
                     title={popupInfo.title}
                     info={popupInfo.info}
+                    tech={popupInfo.tech}
+                    image={popupInfo.image}
                     onClick={closepopup}
                 />
             )}
